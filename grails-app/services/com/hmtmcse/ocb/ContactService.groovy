76183583e09cda2076cc612d6e15b7d6e282c239
YagoGomesDaSilva <yago.gomes.silva.ygs@gmail.com>
2025-12-19 01:08:58 -0300

    Adicionar lógica para upload de imagem de contato e refatorar o serviço de contato para incluir essa funcionalidade

--- grails-app/services/com/hmtmcse/ocb/ContactService.groovy
+++ grails-app/services/com/hmtmcse/ocb/ContactService.groovy
@@ -42,6 +42,7 @@ class ContactService {
             if (!contact.hasErrors()){
                 response.isSuccess = true
                 contactDetailsService.createOrUpdateDetails(contact, params)
+                uploadImage(contact, request)
             }
         } else {
             println("ERRO de validação: ${contact.errors}")
@@ -57,6 +58,7 @@ class ContactService {
             if (!contact.hasErrors()){
                 response.isSuccess = true
                 contactDetailsService.createOrUpdateDetails(contact, params)
+                uploadImage(contact, request)
             }
         }
         return response
@@ -93,6 +95,16 @@ class ContactService {
             return false
         }
         return true
+    }
+
+    def uploadImage(Contact contact, HttpServletRequest request){
+        if (request.getFile("contactImage") && !request.getFile("contactImage").filename.equals("")){
+            String image = FileUtil.uploadContactImage(contact.id, request.getFile("contactImage"))
+            if (!image.equals("")){
+                contact.image = image
+                contact.save(flush:true)
+            }
+        }
     }
 
 }
